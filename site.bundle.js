a947ab0ff7b5bc54f7b7aff810871d2f664ac0c2
WESSELKRAMER <info@wesselkramer.nl>
2026-09-07 15:11:49 +0200

    Update site.bundle.js

--- site.bundle.js
+++ site.bundle.js
@@ -263,10 +263,10 @@ document.querySelectorAll('[data-team-slider]').forEach((sliderEl) => {
   const slider = new KeenSlider(sliderEl, {
     loop: true,
     centered: true,
-    slides: { perView: 1.2, spacing: 0 },
+    slides: { perView: 2.75, spacing: 0 },
     breakpoints: {
-      '(min-width: 768px)': { slides: { perView: 1.6, spacing: 0 } },
-      '(min-width: 1200px)': { slides: { perView: 2, spacing: 0 } }
+      '(min-width: 768px)': { slides: { perView: 2.2, spacing: 0 } },
+      '(min-width: 1200px)': { slides: { perView: 2.75, spacing: 0 } }
     },
     slideChanged: updateActive
   });
