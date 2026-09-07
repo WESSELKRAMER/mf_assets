6166d284f1e529be125ba102750783756851d070
WESSELKRAMER <info@wesselkramer.nl>
2026-09-07 15:10:59 +0200

    Update site.bundle.js

--- site.bundle.js
+++ site.bundle.js
@@ -261,14 +261,15 @@ document.querySelectorAll('[data-team-slider]').forEach((sliderEl) => {
   };
 
   const slider = new KeenSlider(sliderEl, {
-  loop: true,
-  centered: true,
-  slides: { perView: 1.2, spacing: 0 },
-  breakpoints: {
-    '(min-width: 768px)': { slides: { perView: 1.6, spacing: 0 } },
-    '(min-width: 1200px)': { slides: { perView: 2, spacing: 0 } }
-  },
-  slideChanged: updateActive
-});
+    loop: true,
+    centered: true,
+    slides: { perView: 1.2, spacing: 0 },
+    breakpoints: {
+      '(min-width: 768px)': { slides: { perView: 1.6, spacing: 0 } },
+      '(min-width: 1200px)': { slides: { perView: 2, spacing: 0 } }
+    },
+    slideChanged: updateActive
+  });
 
-updateActive(slider);
+  updateActive(slider);
+});
